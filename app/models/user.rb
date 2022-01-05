b5210d5b47f002c59cefefd333d7069036debb14
Nair Designs` <nairbdg@gmail.com>
2022-01-05 22:34:54 +0900

    devise not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord 
-    # devise :database_authenticatable, :registerable,
-    #         :recoverable, :memorable, :validatable
+    devise :database_authenticatable, :registerable,
+            :recoverable, :memorable, :validatable
+    #devise not working
     has_many :projects
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # devise_for :users 
+  # devise_for :users  
   root to: 'projects#index'
 end

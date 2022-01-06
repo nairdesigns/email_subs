5139c1c0e6e3ade61853aa9c85220e9fbe3eaee8
Nair Designs` <nairbdg@gmail.com>
2022-01-06 13:58:30 +0900

    authenticate user! not working!

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
+  # before_action :authenicate_user!
 
   # GET /tasks or /tasks.json
   def index
